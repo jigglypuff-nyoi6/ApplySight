659a46f86b13504afd3c15286037dbe40440a67e
Lis Medina Frey <lismedina@protonmail.com>
2023-09-20 06:29:24 -0400

    Initial Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,9 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+  entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
-    path: __dirname + '/dist',
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  devServer: {
-    contentBase: './dist',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
 };
