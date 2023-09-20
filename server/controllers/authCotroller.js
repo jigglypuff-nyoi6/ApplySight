eccc1cf59f208df041b602c737fe07c41f21344a
Timothy <timevans951@gmail.com>
2023-09-20 16:04:22 -0400

    route change

--- server/controllers/authCotroller.js
+++ server/controllers/authCotroller.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require('express');
 
-export const register = async (req: Request, res: Response) => {
+export const register = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const existingUser = await pool.query(
@@ -16,11 +16,13 @@ export const register = async (req: Request, res: Response) => {
       hashedPassword,
     ]);
     res.status(201).json({ message: 'User registered successfully' });
+
+    // next??
   } catch (err) {
     res.status(500).json({ message: 'Internal server error' });
   }
 };
-export const login = async (req: Request, res: Response) => {
+export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
     const result = await pool.query('SELECT * FROM users WHERE email = $1', [
@@ -30,7 +32,7 @@ export const login = async (req: Request, res: Response) => {
     if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
-    TODO: Implement JWT or session-based authentication to keep the user logged in
+    //  TODO: Implement JWT or session-based authentication to keep the user logged in
     res.status(200).json({ message: 'Login successful' });
   } catch (err) {
     res.status(500).json({ message: 'Internal server error' });
